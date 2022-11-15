15c0423681ef31c6c31fee65268c43bea4c707b8
PRAVIN <pravin@PRAVINs-MacBook-Air.local>
2022-11-15 17:29:43 +0530

    updated

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ app.use(express.urlencoded({extended:false}))
 app.use((req, res, next)=>{
    res.header("Access-Control-Allow-Origin", "*")
    res.header("Access-Control-Allow-Headers","Origin, X-Requested-Width, Content-Type, Accept, Authorization")
-   if(req.method === 'OPTIONS'){
-      res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
-      return res.status(200).json({})
-   }
+   // if(req.method === 'OPTIONS'){
+   //    res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
+   //    return res.status(200).json({})
+   // }
 })
 app.use(require('./routes/auth'))
 
