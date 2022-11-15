069c002e2cc31097874f8ea232d9b62e192c8503
PRAVIN <pravin@PRAVINs-MacBook-Air.local>
2022-11-15 17:33:38 +0530

    updated

--- app.js
+++ app.js
@@ -8,14 +8,20 @@ const User = require('./models/user')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use((req, res, next)=>{
-   res.header("Access-Control-Allow-Origin", "*")
-   res.header("Access-Control-Allow-Headers","Origin, X-Requested-Width, Content-Type, Accept, Authorization")
-   // if(req.method === 'OPTIONS'){
-   //    res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
-   //    return res.status(200).json({})
-   // }
-})
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions))
+// app.use((req, res, next)=>{
+//    res.header("Access-Control-Allow-Origin", "*")
+//    res.header("Access-Control-Allow-Headers","Origin, X-Requested-Width, Content-Type, Accept, Authorization")
+//    // if(req.method === 'OPTIONS'){
+//    //    res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
+//    //    return res.status(200).json({})
+//    // }
+// })
 app.use(require('./routes/auth'))
 
 
