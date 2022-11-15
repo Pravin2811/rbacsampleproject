20cf1ae692b4963670fe92fe5e479d40e5a115af
PRAVIN <pravin@PRAVINs-MacBook-Air.local>
2022-11-15 17:25:08 +0530

    updated

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const User = require('./models/user')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-// app.use((req, res, next)=>{
-//    res.header("Access-Control-Allow-Origin", "*")
-//    res.header("Access-Control-Allow-Headers","Origin, X-Requested-Width, Content-Type, Accept, Authorization")
-//    if(req.method === 'OPTIONS'){
-//       res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
-//       return res.status(200).json({})
-//    }
-// })
+app.use((req, res, next)=>{
+   res.header("Access-Control-Allow-Origin", "*")
+   res.header("Access-Control-Allow-Headers","Origin, X-Requested-Width, Content-Type, Accept, Authorization")
+   if(req.method === 'OPTIONS'){
+      res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
+      return res.status(200).json({})
+   }
+})
 app.use(require('./routes/auth'))
 
 
